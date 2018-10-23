ac974b115252a417df61a6bc3a4ce8d281c64fe2
sunian <sunian@tuotuonet.com>
2018-10-23 11:35:35 +0800

    删除静态库

--- YunejianBuyer/YunejianBuyer/Supporting Files/main.m
+++ YunejianBuyer/YunejianBuyer/Supporting Files/main.m
@@ -9,17 +9,12 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-//#import "person.h"
-//#import <framework_test/framework_test.h>
 
 CFAbsoluteTime StartTime;
 
 int main(int argc, char * argv[]) {
     StartTime = CFAbsoluteTimeGetCurrent();
     @autoreleasepool {
-//        [person personrun];
-//        [animal_dog dogrun];
-//        [animal_dog dogeat];
         [LanguageManager setupCurrentLanguage];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }

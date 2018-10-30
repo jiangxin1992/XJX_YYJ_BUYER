899c804f0110737f130ef0040acc1a8f65886c16
yyj <zhu.guojun@yunejian.com>
2018-10-30 20:17:40 +0800

    删除不必要的引用

--- YunejianBuyer/YunejianBuyer/Controllers/Index/CustomView/YYIndexHotBrandCell.m
+++ YunejianBuyer/YunejianBuyer/Controllers/Index/CustomView/YYIndexHotBrandCell.m
@@ -22,8 +22,8 @@
 #import "UIImage+Tint.h"
 
 // 自定义类和三方类（ cocoapods类 > model > 工具类 > 其他）
-//#import <YYText.h>
-//#import <YYImage.h>
+#import <YYText.h>
+#import <SDWebImageManager.h>
 
 #import "YYHotDesignerBrandsModel.h"
 
@@ -39,8 +39,8 @@
 @property (nonatomic,strong) SCGIFImageView *iconImg;
 @property (nonatomic,strong) UILabel *brandNameLabel;
 @property (nonatomic,strong) UILabel *brandIntroduceLabel;
-@property (nonatomic,strong) UILabel *brandDesLabel;
-//@property (nonatomic,strong) YYLabel *brandDesLabel;
+//@property (nonatomic,strong) UILabel *brandDesLabel;
+@property (nonatomic,strong) YYLabel *brandDesLabel;
 
 @property (nonatomic,strong) UIButton *connectStatusBtn;
 
@@ -180,17 +180,17 @@
         lastLabelView = label;
     }
 
-    _brandDesLabel = [UILabel getLabelWithAlignment:0 WithTitle:nil WithFont:11 WithTextColor:[UIColor colorWithHex:@"919191"] WithSpacing:0];
-//    _brandDesLabel = [[YYLabel alloc] init];
-//    _brandDesLabel.textAlignment = 0;
-//    _brandDesLabel.font = getFont(11.0f);
-//    _brandDesLabel.textColor = [UIColor colorWithHex:@"919191"];
-//    _brandDesLabel.numberOfLines = 0;
+//    _brandDesLabel = [UILabel getLabelWithAlignment:0 WithTitle:nil WithFont:11 WithTextColor:[UIColor colorWithHex:@"919191"] WithSpacing:0];
+    _brandDesLabel = [[YYLabel alloc] init];
+    _brandDesLabel.textAlignment = 0;
+    _brandDesLabel.font = getFont(11.0f);
+    _brandDesLabel.textColor = [UIColor colorWithHex:@"919191"];
+    _brandDesLabel.numberOfLines = 0;
     [_brandView addSubview:_brandDesLabel];
     [_brandDesLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(10);
         make.right.mas_equalTo(-10);
-//        make.bottom.mas_equalTo(0);
+        make.bottom.mas_equalTo(0);
         make.top.mas_equalTo(_iconImg.mas_bottom).with.offset(10);
     }];
     _brandDesLabel.numberOfLines = 2;
@@ -335,16 +335,6 @@
     
     _brandIntroduceLabel.text = [[NSString alloc] initWithFormat: NSLocalizedString(@"%ld 个系列  %ld 个款式",nil),[_hotDesignerBrandsModel.seriesCount integerValue],[_hotDesignerBrandsModel.styleCount integerValue]];
 
-    // 嵌入 UIImage
-//    UIImage *image = [UIImage imageNamed:@"chooseStyle_Select"];
-//    NSMutableAttributedString *attachment = [NSMutableAttributedString yy_attachmentStringWithContent:image contentMode:UIViewContentModeLeft attachmentSize:image.size alignToFont:getFont(11.0f) alignment:YYTextVerticalAlignmentCenter];
-//    NSMutableAttributedString *attachment1 = [[NSMutableAttributedString alloc] initWithString:_hotDesignerBrandsModel.brandDescription];
-//    NSMutableAttributedString *text = [NSMutableAttributedString new];
-//    [text appendAttributedString: attachment];
-//    [text appendAttributedString: attachment1];
-//    _brandDesLabel.attributedText = text;
-    _brandDesLabel.text = _hotDesignerBrandsModel.brandDescription;
-
     //关系类型 -1 没有关系  0 我已发送邀请，对方未处理  1 已为好友   2 对方已发出邀请，我未处理
     NSString *connedImg = @"brandadd_icon";
     _connectStatusBtn.userInteractionEnabled = YES;
@@ -357,10 +347,35 @@
         _connectStatusBtn.userInteractionEnabled = NO;
     }
     [_connectStatusBtn setImage:[UIImage imageNamed:connedImg] forState:UIControlStateNormal];
+
+
+    NSString *imageUrl = @"https://attachments.tower.im/tower/4ee4779076864896adf3fcceb9572938/?filename=image.png";
+    if([NSString isNilOrEmpty:imageUrl]){
+        [self updateBrandDesLabelInfo:nil];
+    }else{
+        SDWebImageManager *manager = [SDWebImageManager sharedManager];
+        [manager loadImageWithURL:[NSURL URLWithString:imageUrl] options:SDWebImageRetryFailed|SDWebImageLowPriority progress:nil completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
+            [self updateBrandDesLabelInfo:image];
+        }];
+    }
+
     DIFFTIME
     NSLog(@"111");
 }
+-(void)updateBrandDesLabelInfo:(UIImage *)image{
+    NSMutableAttributedString *text = [NSMutableAttributedString new];
 
+    if(image){
+        // 嵌入 UIImage
+        NSMutableAttributedString *image_attachment = [NSMutableAttributedString yy_attachmentStringWithContent:image contentMode:UIViewContentModeLeft attachmentSize:image.size alignToFont:getFont(11.0f) alignment:YYTextVerticalAlignmentCenter];
+        [text appendAttributedString: image_attachment];
+
+    }
+    NSMutableAttributedString *des_attachment = [[NSMutableAttributedString alloc] initWithString:_hotDesignerBrandsModel.brandDescription];
+    [text appendAttributedString: des_attachment];
+    _brandDesLabel.attributedText = text;
+    //    _brandDesLabel.text = _hotDesignerBrandsModel.brandDescription;
+}
 #pragma mark - --------------other----------------------
 - (void)awakeFromNib {
     [super awakeFromNib];
